1dacd7a697768989fd1ac3ab6ac6758b0b8f87b9
jiniousKim <size_chi@naver.com>
2020-04-22 18:49:28 +0900

    .

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 0
+    }
+  },
+  name: 'EventCard',
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
